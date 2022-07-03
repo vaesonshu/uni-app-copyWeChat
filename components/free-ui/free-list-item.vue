7c09f6181157ec79c30b50e1d6eeebf8d704347c
vaesonshu <w857669126@gmail.com>
2022-07-03 23:37:19 +0800

    feat(page): 增加通讯录页面

---
+++ components/free-ui/free-list-item.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="bg-white flex align-stretch" hover-class="bg-hover-light" @click="$emit('click')">
+		<view class="flex align-center justify-center py-2 px-3">
+			<image :src="cover" v-if="cover" mode="widthFix" style="width: 75rpx;height: 75rpx;"></image>
+		</view>
+		<view class="flex-1 border-bottom flex align-center">
+			<text class="font-md text-dark">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			// 封面
+			cover: {
+				type: String,
+				default: ""
+			},
+			// 标题
+			title: {
+				type: String,
+				default: ""
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- pages/tabbar/mail/mail.vue
+++ pages/tabbar/mail/mail.vue
@@ -1,14 +1,77 @@
 <template>
 	<view>
+		<!-- 导航栏 -->
+		<free-nav-bar title="通讯录"></free-nav-bar>
+		
+		<!-- 通讯录列表 -->
+		<free-list-item v-for="(item, index) in topList" :key="index" :cover="item.cover" :title="item.title"></free-list-item>
+		
+		<block v-for="(item, index) in list" :key="index">
+			<view class="" v-if="item.data.length">
+				<view class="py-2 px-3 border-bottom bg-light">
+					<text class="font-md text-dark">{{item.letter}}</text>
+				</view>
+				<free-list-item v-for="(item2, index2) in item.data" :key="index2" cover="/static/images/mail/tag.png" :title="item2"></free-list-item>
+			</view>
+		</block>
 		
 	</view>
 </template>
 
 <script>
+	import freeNavBar from '@/components/free-ui/free-nav-bar.vue'
+	import freeListItem from '@/components/free-ui/free-list-item.vue'
 	export default {
+		components: {
+			freeNavBar,
+			freeListItem
+		},
 		data() {
 			return {
-				
+				topList: [
+					{
+						title: "新的朋友",
+						cover: "/static/images/mail/friend.png",
+						event: ""
+					},
+					{
+						title: "群聊",
+						cover: "/static/images/mail/group.png",
+						event: ""
+					},
+					{
+						title: "标签",
+						cover: "/static/images/mail/tag.png",
+						event: ""
+					}
+				],
+				list: [
+					{
+						"letter": "A",
+						"data": [
+							"阿明",
+							"阿郎"
+						]
+					},
+					{
+						"letter": "B",
+						"data": [
+							"别具一格",
+							"别致"
+						]
+					},
+					{
+						"letter": "C",
+						"data": [
+							"参考",
+							"参谋"
+						]
+					},
+					{
+						"letter": "D",
+						"data": []
+					},
+				]
 			}
 		},
 		methods: {
