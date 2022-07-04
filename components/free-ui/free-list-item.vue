6c66053855b7a55e834436be5ab3b9f34f8c7f0c
vaesonshu <w857669126@gmail.com>
2022-07-04 23:49:55 +0800

    feat(page): 增加发现页面

--- components/free-ui/free-list-item.vue
+++ components/free-ui/free-list-item.vue
@@ -1,10 +1,18 @@
 <template>
 	<view class="bg-white flex align-stretch" hover-class="bg-hover-light" @click="$emit('click')">
-		<view class="flex align-center justify-center py-2 px-3">
-			<image :src="cover" v-if="cover" mode="widthFix" style="width: 75rpx;height: 75rpx;"></image>
+		<view class="flex align-center justify-center py-2 pl-3" v-if="showLeftIcon">
+			<slot name="icon"></slot>
+			<image :src="cover" v-if="cover" mode="widthFix" :style="coverStyle"></image>
 		</view>
-		<view class="flex-1 border-bottom flex align-center">
-			<text class="font-md text-dark">{{title}}</text>
+		<view class="flex-1 flex align-center justify-between pr-3 pl-3" :class="border ? 'border-bottom' : ''">
+			<slot>
+				<text class="font-md text-dark">{{title}}</text>
+			</slot>
+			<view class="flex align-center" v-if="showRight">
+				<slot name="right"></slot>
+				<!-- 右箭头 -->
+				<text v-if="showRightIcon" class="iconfont text-light-muted font-md">&#xe60c;</text>
+			</view>
 		</view>
 	</view>
 </template>
@@ -12,6 +20,15 @@
 <script>
 	export default {
 		props: {
+			coverSize: {
+				type: Number,
+				default: 75
+			},
+			// 是否开启下边线
+			border:{
+				type:Boolean,
+				default:true
+			},
 			// 封面
 			cover: {
 				type: String,
@@ -21,8 +38,28 @@
 			title: {
 				type: String,
 				default: ""
+			},
+			// 显示右边
+			showRight:{
+				type:Boolean,
+				default:false
+			},
+			// 显示左边图标
+			showLeftIcon:{
+				type:Boolean,
+				default:true
+			},
+			// 是否显示箭头
+			showRightIcon:{
+				type:Boolean,
+				default:true
 			}
-		}
+		},
+		computed: {
+			coverStyle() {
+				return `width: ${this.coverSize}rpx;height: ${this.coverSize}rpx;`
+			}
+		},
 	}
 </script>
 
--- pages/tabbar/find/find.vue
+++ pages/tabbar/find/find.vue
@@ -1,11 +1,47 @@
 <template>
-	<view>
-		
+	<view class="page">
+		<!-- 导航栏 -->
+		<free-nav-bar title="发现"></free-nav-bar>
+		<free-list-item title="朋友圈" showRight>
+			<text slot="icon" class="iconfont font-lg" style="font-size: 40rpx;padding-top: 10rpx;">&#xe667;</text>
+			<view slot="right" class="position-relative p-1">
+				<free-avatar src="/static/images/mail/friend.png" size="55"></free-avatar>
+				<text class="rounded-circle bg-danger position-absolute" style="width: 20rpx;height: 20rpx;top: 0;right: 0;"></text>
+			</view>
+		</free-list-item>
+		<free-divider></free-divider>
+		<free-list-item title="朋友圈" showRight>
+			<text slot="icon" class="iconfont font-lg py-1">&#xe667;</text>
+		</free-list-item>
+		<free-list-item title="朋友圈" showRight>
+			<text slot="icon" class="iconfont font-lg py-1">&#xe667;</text>
+		</free-list-item>
+		<free-divider></free-divider>
+		<free-list-item title="朋友圈" showRight>
+			<text slot="icon" class="iconfont font-lg py-1">&#xe667;</text>
+		</free-list-item>
+		<free-list-item title="朋友圈" showRight>
+			<text slot="icon" class="iconfont font-lg py-1">&#xe667;</text>
+		</free-list-item>
+		<free-divider></free-divider>
+		<free-list-item title="朋友圈" showRight>
+			<text slot="icon" class="iconfont font-lg py-1">&#xe667;</text>
+		</free-list-item>
 	</view>
 </template>
 
 <script>
+	import freeNavBar from '@/components/free-ui/free-nav-bar.vue'
+	import freeListItem from '@/components/free-ui/free-list-item.vue'
+	import freeAvatar from '@/components/free-ui/free-avatar.vue'
+	import freeDivider from '@/components/free-ui/free-divider.vue'
 	export default {
+		components: {
+			freeNavBar,
+			freeListItem,
+			freeAvatar,
+			freeDivider
+		},
 		data() {
 			return {
 				
